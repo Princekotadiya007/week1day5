0c6c69f85f42ae55e742aafcf841c9b03519cfec
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-10 21:12:40 +0530

    Add files via upload

---
+++ Q9_AlphabetVowelConsonst/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q9_AlphabetVowelConsonst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter ;
+            letter = (Char)Console.Read();
+
+            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
+                letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+            {
+                Console.WriteLine("The letter is a vowel.");
+            }
+            else
+            {
+                Console.WriteLine("The letter is a consonant.");
+            }
+          
+        }
+    }
+}
