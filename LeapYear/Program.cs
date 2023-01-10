98becc7612819edfdcb11a54189196f8d93e47e4
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-10 13:03:49 +0530

    Add files via upload

---
+++ LeapYear/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the input year
+            Console.Write("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            // Check if the year is a leap year
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            {
+                Console.WriteLine("The year {0} is a leap year.", year);
+            }
+            else
+            {
+                Console.WriteLine("The year {0} is not a leap year.", year);
+            }
+        }
+    }
+}
