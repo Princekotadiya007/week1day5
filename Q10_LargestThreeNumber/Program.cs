995ac12de7217afd559770ce06a3a6c3dc26c95f
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-10 21:13:24 +0530

    Add files via upload

---
+++ Q10_LargestThreeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q10_LargestThreeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number:");
+            int C = Convert.ToInt32(Console.ReadLine());
+
+            if(A>B && A>C)
+            {
+                Console.WriteLine("First : is largest number is among three number");
+            }
+            else if (B>A && B>C)
+            {
+                Console.WriteLine("Second : is largest number is among three number");
+            }
+            else
+            {
+                Console.WriteLine("Third : is largest number is among three number");
+            }
+        }
+    }
+}
