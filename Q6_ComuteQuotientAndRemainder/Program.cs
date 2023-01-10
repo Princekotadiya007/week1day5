09cbd817fb2e81dc70c6848cff323b037d8ed31d
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-10 19:32:25 +0530

    Add files via upload

---
+++ Q6_ComuteQuotientAndRemainder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6_ComuteQuotientAndRemainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the dividend and divisor
+            int dividend ;
+            int divisor ;
+
+            Console.WriteLine("enter number devidend :");
+            dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter number divisor :");
+            divisor = Convert.ToInt32(Console.ReadLine());
+
+            // Compute the quotient and remainder
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            // Print the results
+            Console.WriteLine("Quotient: " + quotient);
+            Console.WriteLine("Remainder: " + remainder);
+        }
+    }
+}
